18d52ff9cc7adf0e9cba80ddfec21aa9da8ad556
tahsin.yazkan <tahsin.yazkan@immfly.com>
2025-02-04 15:42:30 +0300

    Add SASS and cleanup sample layout. Create basic table with placeholder values.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.scss';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Movies Database</h1>
+      </header>
+      <main>
+        <table aria-describedby="movie-table-description">
+          <caption>Movie Listing</caption>
+          <thead>
+            <tr>
+              <th scope="col">Movie Title</th>
+              <th scope="col">Year</th>
+              <th scope="col">Imdb ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Year</td>
+              <td>Imdb ID</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
